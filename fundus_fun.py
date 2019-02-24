c18a9c0de867c3dbd395389ba5253b6c99fa02d3
Andy Schneider <andyschndr@gmail.com>
2019-02-24 02:11:46 -0700

    Wow, lots of stuff!
    Added:
     - Function to compute the vesselness at each point in an image
     - Function to segment the vesselness-scored image based on a threshold
     - Function to use the segmented image to compute a bunch of metrics and
       make a visualization
    
    TODO:
     - Find lots of ways to fine-tune to improve performance
     - Automate the processing of a bunch of images in a folder
     - Nicely display the outputs of processing
    
    Ideas:
     - Try finding the indices of areas outside the area of index and fix
     those vesselness scores to 0 (could be done by just looking for dark
     pixels)
     - adjust beta
     - adjust c
     - try getting vesselness scores from slightly smaller scales
       - oh oh, and only accepting them if the high-scoring regions are
         connected to a high-scoring region from a higher-scaled image

--- fundus_fun.py
+++ fundus_fun.py
@@ -1,15 +1,24 @@
 #! python 3
-from PIL import Image
+import cv2
 import os
+from vesselness_process import *
 from tkinter.filedialog import askopenfilename
 import matplotlib.pyplot as plt
 import traceback, sys
+import numpy as np
 
 '''
 This is a super fun program for finding vessels in an image of a fundus!
 The original image is enhanced and segmented, and scored against a hand-
 segmented image.
 '''
+
+# Should we show the green channel of the image during processing?
+debug_original_img = False
+debug_green_img = False
+debug_vesselness_score = True
+debug_segmented_image = True
+debug_segment_score_image = True
 
 root_dir = cwd = os.getcwd()
 default_images_folder = os.path.join(root_dir, "images")
@@ -23,16 +32,118 @@ class Image_Container:
     forms it takes as a result of processing. The actual processing
     is done by other functions or classes.
     '''
-    def __init__(self, raw_image, label_image):
-        self.raw_image = raw_image      # Just the raw image right off the disk
-        self.label_image = label_image  # The corresponding hand-labelled image.
 
-        self.vesselness_score = None    # single-channel 'image' containing vesselness scores
-        self.enhanced_image = None      # RGB-channel enhanced version of the raw image.
-        self.segmented_image = None     # Binary image containing segmentation information
-        self.segment_score_image = None # RGB-channel colourized version of the scored segmented image.
-                                        # true positives are white, true negatives black, false positives blue,
-                                        # false negatives red.
+
+    def __init__(self, raw_image, label_image):
+        self.raw_image = raw_image       # Just the raw image right off the disk
+        self.label_image = label_image.astype(np.uint8)  # The corresponding hand-labelled image.
+
+        self.enhanced_image = None       # RGB-channel enhanced version of the raw image.
+        self.segmented_image = None      # Binary image containing segmentation information
+        self.segment_score_image = None  # RGB-channel colourized version of the scored segmented image.
+                                         # true positives are white, true negatives black, false positives blue,
+                                         # false negatives red.
+        self.vesselness_score = None     # single-channel 'image' containing vesselness scores
+        self.vesselness_threshold = 0.64 # Threshold at which we call the vesselness score a vessel!
+
+        self.true_pos_colour  = [255,255,255] # white
+        self.true_neg_colour  = [0  ,0  ,0  ] # black
+        self.false_pos_colour = [0  ,0  ,255] # blue
+        self.false_neg_colour = [255,0  ,0  ] # red
+
+    def find_vesselness_image(self):
+        '''
+        Compute the vesselness score at each point in the image!
+        '''
+
+        # Extract only the green channel of the raw image for processing
+        self.green_image = self.raw_image[:,:,1]
+        if(debug_green_img):
+            plt.figure()
+            plt.imshow(self.green_image)
+            plt.title('Green Channel only')
+            plt.show()
+
+        # Compute the vesselness of the image!
+        self.vesselness_score = compute_vesselness_multiscale(self.green_image)
+
+        if(debug_vesselness_score):
+            plt.imshow(self.vesselness_score)
+            plt.title('Vesselness score - overall')
+            plt.colorbar()
+            plt.show()
+
+    def generate_segmented_image(self):
+        '''
+        Uses a threshold and the vesselness_score image to create
+        a segmentation
+        '''
+        self.segmented_image = np.copy(self.vesselness_score)
+        above_threshold_indices = self.segmented_image >= self.vesselness_threshold
+        below_threshold_indices = self.segmented_image < self.vesselness_threshold
+        self.segmented_image[above_threshold_indices] = 255
+        self.segmented_image[below_threshold_indices] = 0
+
+        self.segmented_image = self.segmented_image.astype(np.uint8)
+        if(debug_segmented_image):
+            plt.imshow(self.segmented_image, cmap='gray')
+            plt.title('Vesselness segmentation')
+            plt.show()
+
+    def score_segmented_image(self):
+        '''
+        Uses the segmented image and the ground truth image
+        to generate scores for accuracy, specificity, and sensitivity.
+        Then make a pretty image visualizing those scores!
+        '''
+        # Create a blank RGB image
+        self.segment_score_image = np.zeros((self.segmented_image.shape[0], \
+                                   self.segmented_image.shape[1],3), np.uint8)
+        label = cv2.cvtColor(self.label_image, cv2.COLOR_RGB2GRAY)
+        self.false_pos = 0
+        self.false_neg = 0
+        self.true_pos  = 0
+        self.true_neg  = 0
+
+        # Run through each pixel and make a note of true positives,
+        # false positives, false negatives, and true negatives.
+        for x, y in np.ndindex(self.segmented_image.shape):
+            if(self.segmented_image[x,y] == 255 and label[x,y] == 255 ):
+                self.true_pos = self.true_pos + 1
+                self.segment_score_image[x,y] = self.true_pos_colour
+
+            elif(self.segmented_image[x,y] == 0 and label[x,y] == 0 ):
+                self.true_neg = self.true_neg + 1
+                self.segment_score_image[x,y] = self.true_neg_colour
+
+            elif(self.segmented_image[x,y] == 255 and label[x,y] == 0 ):
+                self.false_pos = self.false_pos + 1
+                self.segment_score_image[x,y] = self.false_pos_colour
+
+            elif(self.segmented_image[x,y] == 0 and label[x,y] == 255 ):
+                self.false_neg = self.false_neg + 1
+                self.segment_score_image[x,y] = self.false_neg_colour
+
+        self.accuracy    = (self.true_neg + self.true_pos) / \
+                           (self.true_neg + self.true_pos + self.false_neg + self.false_pos)
+        self.sensitivity = (self.true_pos) / (self.true_pos + self.false_neg)
+        self.specificity = (self.true_neg) / (self.true_neg + self.false_pos)
+
+        if(debug_segment_score_image):
+            plt.imshow(self.segment_score_image)
+            plt.title(('Segmentation score : Accuracy = {0:.4f} : Sensitivity = {1:.4f} : ' + \
+                       'Specificity = {2:.4f}').format(self.accuracy, self.sensitivity, self.specificity))
+            plt.show()
+
+    def generate_enhanced_image(self):
+        '''
+        Use the vesselness scores and the original image
+        to enhance the areas where there are vessels!
+        '''
+        self.enhanced_image
+
+
+
 
 class Fundus_Fun_App:
     def __init__(self):
@@ -45,29 +156,36 @@ class Fundus_Fun_App:
         image_path = os.path.join(images_folder, image_file)
         label_path = os.path.join(labels_folder, label_file)
 
-        image = self.load_image(image_path)
-        label = self.load_image(label_path)
+        image = self.load_image(image_path, True)
+        label = self.load_image(label_path, False)
 
         image_container = Image_Container(image, label)
 
         # Simply display using matplotlib.
-        plt.subplots(2,1)
-        plt.subplot(2,1,1)
-        plt.imshow(image_container.raw_image)
-        plt.title(image_file + ' - Image')
-        plt.subplot(2,1,2)
-        plt.imshow(image_container.label_image)
-        plt.title(label_file + ' - Label')
-        plt.show()
+        if(debug_original_img):
+            plt.subplots(2,1)
+            plt.subplot(2,1,1)
+            plt.imshow(image_container.raw_image)
+            plt.title(image_file + ' - Image')
+            plt.subplot(2,1,2)
+            plt.imshow(image_container.label_image)
+            plt.title(label_file + ' - Label')
+            plt.show()
 
-    def load_image(self, image_filepath=None):
+        image_container.find_vesselness_image()
+        image_container.generate_segmented_image()
+        image_container.score_segmented_image()
+
+    def load_image(self, image_filepath=None, convert_from_bgr=False):
         if(image_filepath is None):
             # Allow the user to directly pick some images if they don't give an argument
             image_filepath = askopenfilename(filetypes = (("ppm images","*.ppm"),("all files","*.*")))
             if not os.path.exists(image_filepath):
                 raise Exception('This file does not exist: {}'.format(image_filepath))
 
-        image = Image.open(image_filepath)
+        image = cv2.imread(image_filepath)
+        if(convert_from_bgr):
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         return image
 
 
@@ -80,5 +198,5 @@ if __name__ == '__main__':
         traceback.print_tb(exc_traceback, limit=1, file=sys.stdout)
         # print exception
         traceback.print_exception(exc_type, exc_value, exc_traceback,
-                              limit=2, file=sys.stdout)
+                                limit=2, file=sys.stdout)
 
---
+++ vesselness_process.py
@@ -0,0 +1,80 @@
+#! Python 3
+import numpy as np
+from math import sqrt, exp
+import matplotlib.pyplot as plt
+from skimage.feature import hessian_matrix, hessian_matrix_eigvals
+# The set of scales at which to run the vesselness scoring
+#vesselness_scales = [0.1,0.2,0.3,0.5,0.8,1.3,2.1,4,5,6,7,8,9,10,11,12,13,14,15,16]
+#vesselness_scales = [0.8,1.3,1.6,2.0,2.4,2.8,3.3,3.8,4.3,4.8,5.3,5.8,6.3,6.8,7.3]
+vesselness_scales = [2.4,2.8,3.3,3.8]
+# beta is a threshold controlling the sensitivity to the blobness measure
+# TODO: goof around with this threshold and see if it helps.
+beta = 0.5
+# c is a threshold controlling the sensitivity to second-order structureness.
+# for now this is just a rough guess, but there may actually be a much better choice for c ;)
+c = (62.0/255.0)
+
+#Show the intermediate images with vesselness at different scales!
+debug_vessel_scores = True
+
+def compute_vesselness_multiscale(image):
+    '''
+    Run the vesselness computation at several different scales,
+    and take the highest score!
+    '''
+    image = image.astype(np.double)
+    vesselness_scores = []
+    final_scores = np.zeros((image.shape[0], image.shape[1]))
+
+    # Run through each scale and compute the vesselness score for the image at that scale
+    for scale in vesselness_scales:
+        scale_scores = compute_vesselness(image, scale)
+        vesselness_scores.append(scale_scores)
+        final_scores = np.maximum(scale_scores, final_scores)
+
+    # If you like, show an image of what the scores look like at all the different scales
+    if(debug_vessel_scores):
+        plt.subplots(2,2)
+        i = 1
+        for score in vesselness_scores:
+            plt.subplot(2,2,i)
+            plt.imshow(score)
+            plt.title('Vesseness - Scale = ' + str(vesselness_scales[i-1]))
+            plt.colorbar()
+            i += 1
+
+    plt.show()
+    return final_scores
+
+def compute_vesselness(image, scale):
+    '''
+    Compute the vesselness at a specific scale.
+    '''
+    # find the hessian at a certain scale
+    hessian = hessian_matrix(image, sigma=scale)
+    hessian_eigenvalues = hessian_matrix_eigvals(hessian)
+
+    scores = np.zeros((image.shape[0], image.shape[1]))
+
+    # Run through each pixel in the image
+    for x, y in np.ndindex(image.shape):
+        # in hessian_eigenvalues, the values are ordered from largest to smallest,
+        # but we want the first eigenvalue to be the smallest.
+        eigenvalue_xy_1 = hessian_eigenvalues[1,x,y]
+        eigenvalue_xy_2 = hessian_eigenvalues[0,x,y]
+
+        vesselness = 0.0
+        if(eigenvalue_xy_2 > 0):
+
+            # Blobness measure
+            R_beta = eigenvalue_xy_1 / eigenvalue_xy_2
+            # Second-order structureness
+            so_structureness = sqrt(eigenvalue_xy_1**2 + eigenvalue_xy_2**2)
+
+            # Vesselness scoring function in 2d credit to A. Frangi et al.
+            vesselness = exp(-( (R_beta**2) / ( 2 * (beta**2)))) * \
+                         (1 - exp(-( (so_structureness**2) / ( 2 * (c**2)))))
+
+        scores[x,y] = vesselness
+
+    return scores
